2edee7e8f5ed80c4c942d9963adbfb64e7927d97
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-01-25 17:06:12 +0200

    Correctly returning final promise

--- src/handler.ts
+++ src/handler.ts
@@ -1,42 +1,42 @@
-"use strict"
+"use strict";
 
-import * as sql from "mssql"
-import * as googleMapSdk from "@google/maps"
-import { Context } from "serverless-azure-functions"
-import { ServerResponse } from "http"
-import { Http2ServerResponse } from "http2"
-import { resolve } from "url"
+import * as sql from "mssql";
+import * as googleMapSdk from "@google/maps";
+import { Context } from "serverless-azure-functions";
+import { ServerResponse } from "http";
+import { Http2ServerResponse } from "http2";
+import { resolve } from "url";
 
-const BbPromise = require("bluebird")
+const BbPromise = require("bluebird");
 
 const googleMapsClient = googleMapSdk.createClient({
   key: process.env.GOOGLE_KEY || "123",
   Promise: BbPromise // Trying to use BbPromise, maybe it will not use native promises and execute code on main thread?
-})
+});
 
 /* eslint-disable no-param-reassign */
 async function ignoreExceptions(f: Function, context: any = null) {
   try {
-    f()
+    f();
   } catch (e) {
     if (context != null) {
-      context.log(e)
+      context.log(e);
     } else {
-      console.log(e)
+      console.log(e);
     }
   }
 }
 
 /* Declared during build after TS -> JS compilation */
-declare var BUILD_TIME: string
+declare var BUILD_TIME: string;
 
 async function updateAddresses(context: Context) {
   context.log(
     "Starting updateAddresses, build time: ",
     typeof BUILD_TIME !== "undefined" ? BUILD_TIME : "dev"
-  )
+  );
 
-  var config = {
+  let config = {
     user: process.env.SQL_USER || "123",
     password: process.env.SQL_PASSWORD || "123",
     server: process.env.SQL_SERVER || "localhost",
@@ -45,39 +45,30 @@ async function updateAddresses(context: Context) {
       encrypt:
         process.env.SQL_ENCRYPT !== undefined ? process.env.SQL_ENCRYPT : true
     }
-  }
+  };
 
   context.log(
     "Using config",
     Object.assign({}, config, { password: "***" }),
     "to login to MS-SQL",
     sql
-  )
+  );
 
-  sql.Promise = BbPromise
+  sql.Promise = BbPromise;
 
-  sql
+  let result = sql
     .connect(config)
     .then(_ => {
-      context.log("After connect")
-      const request = new sql.Request()
-      request.query(
-        "select * from Saleslt.Address where Lat = 1 or Lng = 1 or Lat is null or Lng is null",
-        async function(err, result) {
-          if (err) {
-            context.log(err)
-            context.res = {
-              status: 500,
-              body: "Failed query " + err
-            }
-
-            ignoreExceptions(_ => sql.close())
-            return context.done()
-          }
-
-          let recordset = result.recordsets[0]
-          context.log("Total records", recordset.length)
-          let n = 0
+      context.log("After connect");
+      const request = new sql.Request();
+      return request
+        .query(
+          "select * from Saleslt.Address where Lat = 1 or Lng = 1 or Lat is null or Lng is null"
+        )
+        .then(result => {
+          let recordset = result.recordsets[0];
+          context.log("Total records", recordset.length);
+          let n = 0;
           let promises = recordset.map(async r => {
             let addr =
               r.AddressLine1 +
@@ -88,8 +79,9 @@ async function updateAddresses(context: Context) {
               " " +
               r.PostalCode +
               " " +
-              r.CountryRegion
-            context.log("Geocoding", addr)
+              r.CountryRegion;
+            context.log("Geocoding", addr);
+
             return googleMapsClient
               .geocode({
                 address: addr
@@ -101,10 +93,10 @@ async function updateAddresses(context: Context) {
                   geores.json.results &&
                   geores.json.results.length > 0
                 ) {
-                  let res = geores.json.results[0]
-                  let lat = res.geometry.location.lat
-                  let lng = res.geometry.location.lng
-                  context.log("Got address coords", lat, lng)
+                  let res = geores.json.results[0];
+                  let lat = res.geometry.location.lat;
+                  let lng = res.geometry.location.lng;
+                  context.log("Got address coords", lat, lng);
                   request
                     .query(
                       "update Saleslt.Address set Lat = " +
@@ -115,63 +107,78 @@ async function updateAddresses(context: Context) {
                         r.AddressID
                     )
                     .then(_ => {
-                      n++
+                      n++;
                     })
-                    .catch(e => context.log(e))
+                    .catch(e => context.log(e));
                 } else {
-                  context.log("Incomplete response", geores)
+                  context.log("Incomplete response", geores);
                 }
               })
-              .catch(err => context.log("Failed to geocode", err))
-          })
+              .catch(err => context.log("Failed to geocode", err));
+          });
 
-          Promise.all(promises)
+          return Promise.all(promises)
             .then(_ => {
               context.res = {
                 // status: 200, /* Defaults to 200 */
                 body: "Updated " + n + " addresses"
-              }
+              };
 
-              ignoreExceptions(_ => sql.close())
-              context.log("Finished updating addresses")
-              return context.res
+              ignoreExceptions(_ => sql.close());
+              context.log("Finished updating addresses");
+              return context.res;
             })
             .catch(e => {
-              context.log("Failed to run all of updates, got error", e)
+              context.log("Failed to run all of updates, got error", e);
               context.res = {
                 status: 500,
                 body: "Failed to run all updates: " + e
-              }
-              return context.res
-            })
-        }
-      )
+              };
+              return context.res;
+            });
+        })
+        .catch(err => {
+          context.log(err);
+          context.res = {
+            status: 500,
+            body: "Failed query " + err
+          };
+
+          ignoreExceptions(_ => sql.close());
+          return context.res;
+        });
     })
     .catch(err => {
-      context.log("After connect exception")
-      context.log(err)
+      context.log("After connect exception");
+      context.log(err);
       context.res = {
         status: 500,
         body: "Failed to connect to DB: " + err
-      }
+      };
 
-      ignoreExceptions(_ => sql.close())
-      return context.res
-    })
+      ignoreExceptions(_ => sql.close());
+      return context.res;
+    });
 
   // Dump any pending NodeJs eventloop tasks
-  var p = <any>process
+  var p = <any>process;
   context.log(
     "After connect body",
     p._getActiveRequests().length,
     p._getActiveHandles().length
-  )
+  );
+
+  context.log("Result", result);
+  result.then(r => {
+    context.log("Got final promise and its result", r);
+  });
+  return result;
 }
 
-module.exports.updateAddresses = updateAddresses
+module.exports.updateAddresses = updateAddresses;
 
 if (process.env.RUN_CONSOLE) {
-  console.log("Run in console")
+  console.log("Run in console");
   const ctx = {
     log: console.log,
     res: {
@@ -179,8 +186,8 @@ if (process.env.RUN_CONSOLE) {
       body: null
     },
     done: function() {
-      console.log("Done, result: ", ctx.res.status || 200, ctx.res.body)
+      console.log("Done, result: ", ctx.res.status || 200, ctx.res.body);
     }
-  }
-  updateAddresses(ctx)
+  };
+  updateAddresses(ctx);
 }
